fddf04732a4bf3a021febc25b8446be76fac595a
norhane1234567 <norhanelshawarby@gmail.com>
2024-06-18 23:09:12 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if($(window).scrollTop() > 60){
+            $('header .header-2').addClass('header-active');
+        }else{
+            $('header .header-2').removeClass('header-active');
+        }
+
+        $('section').each(function(){
+
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let top = $(window).scrollTop();
+            let id = $(this).attr('id');
+
+            if(top >= offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+
+        });
+
+    });
+
+    $('.controls .buttons').click(function(){
+
+        $(this).addClass('button-active').siblings().removeClass('button-active');
+
+        let filter = $(this).attr('data-filter');
+        if(filter == 'all'){
+            $('.dish .image').show(400);
+        }else{
+            $('.dish .image').not('.'+filter).hide(200);
+            $('.dish .image').filter('.'+filter).show(400);
+        }
+
+    });
+    var swiper = new Swiper(".gallery-slider", {
+        loop:true,
+        effect: "coverflow",
+        slidesPerView: "auto",
+        centeredSlides: true,
+        grabCursor: true,
+        coverflowEffect: {
+           rotate: 0,
+           stretch: 0,
+           depth: 100,
+           modifier: 2,
+           slideShadows: true,
+        },
+        pagination: {
+           el: ".swiper-pagination",
+         },
+     });
+
+});
